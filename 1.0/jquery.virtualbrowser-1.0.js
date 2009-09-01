d0c7507812bdaca26620161239ec3e63d8474915
mar <mar@85455381-3708-0410-a195-d385fbeb4437>
2009-09-01 13:46:46 +0000

    New plugin: $.fn.virtualBrowser();
    git-svn-id: http://svn2.eplica.is/codecentre/trunk@1868 85455381-3708-0410-a195-d385fbeb4437

--- 1.0/jquery.virtualbrowser-1.0.js
+++ 1.0/jquery.virtualbrowser-1.0.js
@@ -1,10 +1,13 @@
 // encoding: utf-8
 /*
   jQuery.fn.virtualBrowser();
-    Turns any element into a virtual browser window (or iframe), attempting to 
+    Turns any element into a virtual browser window (or iframe), attempting to
     capture all link-clicks and form submits and convert them into ajax-requests,
-    and then inserting (and manipulating) the response document into the 
+    and then inserting (and manipulating) the response document into the
     virtualBrowser element (the semantic equivalient of a browser's <body>).
+
+  Requires:
+    * jQuery 1.3.3+
 
 
   Usage/Init:
@@ -15,10 +18,10 @@
   Options:
     * url:         null,                      // String: Initial URL for the frame
     * params:      null,                      // Object/String: Request data (as in $.get(url, data, callback) )
-    * loadmsg:     '<div class="loading" />'  // String/Element: Template for a loading message displayed while loading a URL
-    * loadmsgMode: 'none',                    // String: Options: (none|overlay|replace)
     * onPreload:   null,                      // Function: Shorthand for .bind('vbrowserpreload' handler);
     * onLoad:      null,                      // Function: Shorthand for .bind('vbrowserload' handler);
+    * loadmsgElm:  '<div class="loading" />'  // String/Element: Template for a loading message displayed while loading a URL
+    * loadmsgMode: 'none',                    // String: Options: (none|overlay|replace)
 
   Localization:
     * jQuery.fn.virtualBrowser.i18n['lang'] = {
@@ -28,20 +31,27 @@
 
 
   Events:
-    * 'preload'  // .bind('vbrowserpreload', function (e, elm) {
-                        this              // the virtualBrowser body element
-                        $(this).data('virtualBrowser').cfg // config object
-                        elm  // the link or form element that was clicked/activated
-                        // Cancellable via e.preventDefault()
-                      });
-    * 'load'     // .bind('vbrowserload', function (e, response) {
-                        this                               // the virtualBrowser body element
-                        $(this).data('virtualBrowser').cfg // config object
-                        response                           // { result: '$.ajax()/$.get() result string.' // read by handler
-                                                           //   dom: DOMElementsToInsertIntoVBrowserBody  // set by handler
-                                                           // }
-                        // Cancellable via e.preventDefault()
-                      });
+    * 'VBbeforeload'   // .bind('VBbeforeload', function (e, request) {
+                              this  // the virtualBrowser body element
+                              $(this).data('virtualBrowser').cfg  // config object
+                              request  // Object: {
+                                       //   url:  // String the URL that was just loaded (Modifiable by handler)
+                                       //   elm:  // jQuery collection containing (when applicable) the link (or form element) that was clicked/submitted
+                                       // }
+                              // Cancellable via e.preventDefault()
+                              // e.passThrough = true;  // Instructs the virtualBrowser to disable any click events and pass the click through to the web browser
+                            });
+    * 'VBload'         // .bind('VBload', function (e, request) {
+                              this  // the virtualBrowser body element
+                              $(this).data('virtualBrowser').cfg // config object
+                              request  // Object: {
+                                       //   result:     // String: The $.ajax()/$.get() callback result parameter (Read by handler)
+                                       //   resultDOM:  // Element(s)/Collection to insert into the virtualBrowser body  (Set by handler)
+                                       //   url:  // String the URL that was just loaded
+                                       //   elm:  // jQuery collection containing (when applicable) the link (or form element) that was clicked/submitted
+                                       // }
+                              // Cancellable via e.preventDefault()
+                            });
 
 
   Methods:
@@ -49,59 +59,157 @@
 
 
 
-  TODO/ideas: 
+  TODO/ideas:
     * History buffer
        * Add 'back' (and 'forward'?) methods
     * Consider adding 'reload' method
-       
+
 */
 
 (function($, undefined){
 
-  
-  var methods = {
-      'load': function (url) {
-          
-        }
-      
+  // FIXME: remove this when
+  $.fn.detach = $.fn.detach || function(){
+      return this.each(function(){
+          var parent = this.parentNode;
+          parent  &&  parent.nodeType==1  &&  parent.removeChild(this);
+        });
     };
 
 
-  $.fn.virtualBrowser = function (config, args) {
-      var confIsString = typeof config == 'string';
-      if (confIsString)
-      {
-        var method = methods[config];
-        method  &&  this.each(function(){
-              method.apply( this, [].concat(args) );  // Normalize `args` into an array. ([].concat() does that :-)
-            });
-      }
 
-      if (confIsString)
-      {
-        var body = $(this),
-            lang = ...;
+  var _virtualBrowser = 'virtualBrowser',  // ...to save bandwidth
+      _VBbeforeload   = 'VBbeforeload',    // ...to save bandwidth
+      _VBload         = 'VBload',          // ...to save bandwidth
 
-        config = $.extend(
+
+      _methods = {
+          'load': function (url) {
+              var elm = typeof url != 'string' ? $(url) : undefined,
+                  body = $(this),
+                  config = body.data(_virtualBrowser).cfg,
+                  ev1 = jQuery.Event(_VBbeforeload),
+                  loadmsgMode = config.loadmsgMode,
+                  request = { elm: elm };
+
+              request.url = elm ?
+                                (elm.attr('href') || elm.attr('action') || ''):
+                                url;
+              if (request.url)
+              {
+                body.trigger(ev1, request);
+                if ( !ev1.isDefaultPrevented() )
                 {
-                  //url:         null,                      // String: Initial URL for the frame
-                  //params:      null,                      // Object/String: Request data (as in $.get(url, data, callback) )
-                  loadmsg:     '<div class="loading" />'  // String/Element: Template for a loading message displayed while loading a URL
-                  loadmsgMode: 'none',                    // String: Options: (none|overlay|replace)
-                  //onPreload:   null,                      // Function: Shorthand for .bind('vbrowserpreload' handler);
-                  //onLoad:      null,                      // Function: Shorthand for .bind('vbrowserload' handler);
-                },
-                config || {};
-              );
-        args && (config.url = args);
+                  if (loadmsgMode && loadmsgMode != 'none')
+                  {
+                    config.loadmsgMode == 'replace'  &&  body.empty();
+                    body.append(config.loadmsgElm);
+                  }
+                  var params = config.params,
+                      method;
+                  if (elm && elm.is('form') )
+                  {
+                    params = elm.serialize() + '&' + params;
+                    method = elm.attr('method')||'GET';
+                  }
+                  window.console&&console.log( (method||'Load')+'ing: '+ request.url + (params?'?'+params:'')  );
+                  $.ajax({
+                      url: request.url,
+                      data: params,
+                      type: method,
+                      complete:  function (result) {
+                                    request.result = result;
+                                    var ev2 = jQuery.Event(_VBload);
+                                    body.trigger(ev2, request);
+                                    if ( !ev2.isDefaultPrevented() )
+                                    {
+                                      config.loadmsgElm.detach();
+                                      body
+                                          .empty()
+                                          .append( request.resultDOM || $(request.result) )
+                                          .find('form')
+                                              .data(_virtualBrowser+'Elm', body)
+                                              .bind('submit', _handleRequest);
+                                    }
+                                  }
+                      });
+                }
+              }
+              return ev1;
+            }
 
-      }
-
-      return this;
-    };
+        },
 
 
-  $.fn.virtualBrowser.i18n = {
+      _handleRequest = function (e) {
+          var elm = $(e.target).closest('[href], form')[0];
+          if (elm)
+          {
+            var body = $(this).data(_virtualBrowser+'Elm') || this;
+                bfldEv = _methods['load'].call(body, elm);
+            bfldEv.isPropagationStopped() &&  e.stopPropagation();
+            !bfldEv.passThrough && e.preventDefault();
+          }
+        };
+
+
+
+      fnVB = $.fn[_virtualBrowser] = function (config, args) {
+          var confIsString = typeof config == 'string';
+          if (confIsString)
+          {
+            var method = _methods[config];
+            method  &&  this.each(function(){
+                  method.apply( this, [].concat(args) );  // Normalize `args` into an array. ([].concat() does that :-)
+                });
+          }
+          else
+          {
+            config = $.extend(
+                    {
+                      //url:         null,                      // String: Initial URL for the frame
+                      //params:      null,                      // Object/String: Request data (as in $.get(url, data, callback) )
+                      //onBeforeload:   null,                   // Function: Shorthand for .bind('VBbeforeload' handler);
+                      //onLoad:      null,                      // Function: Shorthand for .bind('VBload' handler);
+                      //loadmsgElm:  '<div class="loading" />'  // String/Element: Template for a loading message displayed while loading a URL
+                      loadmsgMode:    'none'                    // String: available: "none", "overlay" & "replace"
+                    },
+                    config || {}
+                  );
+            args && (config.url = args);
+            var body = $(this),
+                loadmsgElm = config.loadmsgElm || '<div class="loading" />',
+                loadmsg = (fnVB.i18n[body.closest('[lang]').attr('lang')] || {}).loading || fnVB.i18n.en.loading;;
+
+            if (loadmsgElm.charAt)
+            {
+              loadmsgElm = loadmsgElm.replace(/%\{msg\}/g, loadmsg);
+            }
+            loadmsgElm = config.loadmsgElm = $(loadmsgElm);
+            if ( !loadmsgElm.text() )
+            {
+              loadmsgElm.append(loadmsg);
+            }
+
+            body
+                .data(_virtualBrowser, { cfg: config })
+                .bind('click', _handleRequest);
+
+            config.onLoad && body.bind(_VBload, config.onLoad);
+            config.onBeforeload && body.bind(_VBbeforeload, config.onBeforeload);
+            config.params =  typeof config.params == 'string' ?
+                              config.params:
+                              $.param(config.params||{});
+
+            config.url  &&  body[_virtualBrowser]('load', config.url)
+          }
+
+          return this;
+        };
+
+
+
+  fnVB.i18n = {
       en: { loading: 'Loading...' },
       is: { loading: 'Sæki gögn...' }
     }
